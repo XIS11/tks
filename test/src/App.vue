e1b2ded80be4befe273e98d7e031d133117f2171
Ivan Ivanov <naoadvert@yandex.ru>
2020-05-13 22:17:49 +0300

    v02

--- test/src/App.vue
+++ test/src/App.vue
@@ -1,10 +1,63 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <v-app>
+      <div id="nav">
+          <v-app-bar
+            color="white"
+            elevate-on-scroll
+            fixed
+          >
+            <v-toolbar-title>Title</v-toolbar-title>
+            <v-spacer></v-spacer>
+            <router-link to="/" class="mr-5">ГЛАВНАЯ</router-link>
+            <router-link to="/about">О НАС</router-link>
+            <v-btn icon>
+              <v-icon>mdi-magnify</v-icon>
+            </v-btn>
+
+            <v-btn icon>
+              <v-icon>mdi-heart</v-icon>
+            </v-btn>
+
+            <v-btn icon>
+              <v-icon>mdi-dots-vertical</v-icon>
+            </v-btn>
+          </v-app-bar>
+
+      </div>
+      <router-view/>
+      <v-footer
+          dark
+          padless
+        >
+          <v-card
+            flat
+            tile
+            class="red lighten-1 white--text text-center"
+          >
+            <v-card-text>
+              <v-btn
+                v-for="icon in icons"
+                :key="icon"
+                class="mx-4 white--text"
+                icon
+              >
+                <v-icon size="24px">{{ icon }}</v-icon>
+              </v-btn>
+            </v-card-text>
+
+            <v-card-text class="white--text pt-0">
+              Phasellus feugiat arcu sapien, et iaculis ipsum elementum sit amet. Mauris cursus commodo interdum. Praesent ut risus eget metus luctus accumsan id ultrices nunc. Sed at orci sed massa consectetur dignissim a sit amet dui. Duis commodo vitae velit et faucibus. Morbi vehicula lacinia malesuada. Nulla placerat augue vel ipsum ultrices, cursus iaculis dui sollicitudin. Vestibulum eu ipsum vel diam elementum tempor vel ut orci. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+            </v-card-text>
+
+            <v-divider></v-divider>
+
+            <v-card-text class="white--text">
+              {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
+            </v-card-text>
+          </v-card>
+        </v-footer>
+    </v-app>
   </div>
 </template>
 
@@ -30,3 +83,15 @@
   }
 }
 </style>
+<script>
+  export default {
+    data: () => ({
+      icons: [
+        'mdi-facebook',
+        'mdi-twitter',
+        'mdi-linkedin',
+        'mdi-instagram',
+      ],
+    }),
+  }
+</script>
